840df7e4a8a3b64654740a9596b7ce2b5574c1ea
mlkmoaz01 <mlkmoaz01@gmail.com>
2024-10-20 01:06:53 +0500

    FrontEnd

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         <img src={captureLogo} alt="Capture Logo" />
       </div>
       <div className='m-5'>
-      <button className="text-white px-4 py-1 rounded-full bg-blue-600 hover:bg-blue-700 transition duration-300">Login</button>
+      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Login</button>
       <button className="text-white px-4 py-1 rounded-full bg-green-600 hover:bg-green-700 transition duration-300">Sign Up</button>
       </div>
     </div>
