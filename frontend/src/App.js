3d11e92200e47132fc50bfcb0e5d113ff3ea24cd
mlkmoaz01 <mlkmoaz01@gmail.com>
2025-01-05 20:08:09 +0500

    Prototype Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,6 +49,7 @@ import BelowFooter from './Components/User/BelowFooter';
 import UserProfile from './Components/User/UserProfile';
 import OrderHistory from './Components/User/OrderHistory';
 import PaymentMethod from './Components/User/PaymentMethods';
+import ProductOverview from './Components/User/ProductOverview';
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -128,6 +129,7 @@ function App() {
                 <Route path='/account'  element={<UserProfile/>}/>
                 <Route path='/order-history' element={<OrderHistory/>}/>
                 <Route path='/payment-methods' element={<PaymentMethod/>}/>
+                <Route path='/product-overview' element={<ProductOverview/>}/>
                 {/* Menswear */}
                 <Route path="/menswear/shirts" element={<Shirts />} />
                 <Route path="/menswear/tshirts" element={<TShirts />} />
