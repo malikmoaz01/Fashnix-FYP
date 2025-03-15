6cacd44cc02cf3acee92f5e777070e4e5eb1201e
mlkmoaz01 <mlkmoaz01@gmail.com>
2025-03-16 03:48:51 +0500

    Login Code Working Feature 3 Completed

--- backend/routes/signupRoutes.js
+++ backend/routes/signupRoutes.js
@@ -3,7 +3,6 @@ import { signupUser } from "../controllers/signupController.js";
 
 const router = express.Router();
 
-// Signup Route
 router.post("/signup", signupUser);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import contactRoutes from "./routes/contactSend.js";
-
-// const app = express();
-// const PORT = 5000;
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/send-email", contactRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
-
-// export default app;
-
 
 import express from "express";
 import cors from "cors";
