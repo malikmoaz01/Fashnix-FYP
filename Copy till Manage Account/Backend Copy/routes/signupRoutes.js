d0c1581e1763d92354cc8778ada7793f3dadaa37
mlkmoaz01 <mlkmoaz01@gmail.com>
2025-04-29 23:22:52 +0500

    UI is improving

--- Copy till Manage Account/Backend Copy/routes/signupRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signupUser } from "../controllers/signupController.js";
-
-const router = express.Router();
-
-router.post("/signup", signupUser);
-
-export default router;
-  
